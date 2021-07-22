198e4c5442dc0a075847f8ffa9b4a4a96cdbe6c4
Atharva Adhav <adhavatharva@gmail.com>
2021-07-22 18:08:38 +0530

    minor change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./database');
 
 
 const years = ['FY', 'SY', 'TY', 'BE'];
-const departments = ['Computer', 'IT', 'Mechanical', 'E&TC', 'Civil', 'FY'];
+const departments = ['Computer', 'IT', 'Mechanical', 'E&TC', 'Civil'];
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -40,9 +40,7 @@ app.post('/users/register', async (req, res) => {
     let queryUserRes;
     try {
         const queryUser = await db.promise().query(`SELECT * FROM usernames WHERE username='${username}'`);
-        console.log(queryUser);
         queryUserRes = queryUser[0];
-        console.log(queryUserRes);
     } catch (error) {
         console.log(error);
     }
@@ -89,9 +87,6 @@ app.post('/users/login', async (req, res) => {
         res.render('users/loginFailed');
     }
     else if (checkUser.length !== 0 && checkUser[0].password !== password) {
-        console.log(checkUser);
-        console.log(`database password = '${ checkUser.password }'`);
-        console.log(`user password = '${ password }'`);
         console.log('Password incorrect');  
         res.render('users/loginFailed');
     }
